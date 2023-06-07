e45eb29902f3866b8d826612b6239cf65589db80
ym1021 <ym1021@cu.ac.kr>
2023-06-08 03:00:58 +0900

    20230607

---
+++ baekjoon/9461.py
@@ -0,0 +1,12 @@
+p = [0 for _ in range(101)]
+p[1] = 1
+p[2] = 1
+p[3] = 1
+
+for i in range(4, 101):
+    p[i] = p[i-3]+ p[i-2]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(p[n])
